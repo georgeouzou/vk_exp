d5f99d0125fc006f54d268dc3887598a467229a2
georgeouzou <george.ouzou.w@gmail.com>
2018-11-06 22:41:32 +0200

    Add first swapchain, command buffers and presentation

--- src/main.cpp
+++ src/main.cpp
@@ -56,10 +56,10 @@ VkInstance create_instance()
 	return instance;
 }
 
-VkDevice create_device(VkInstance instance)
+VkDevice create_device(VkInstance instance, uint32_t *family_index)
 {
 	std::vector<VkPhysicalDevice> phys_devices(2);
-	uint32_t phys_device_count = phys_devices.size();
+	uint32_t phys_device_count = (uint32_t)phys_devices.size();
 	VK_CHECK(vkEnumeratePhysicalDevices(instance, &phys_device_count, phys_devices.data()));
 
 	VkPhysicalDevice phys_device = choose_physical_device(phys_devices);
@@ -70,15 +70,23 @@ VkDevice create_device(VkInstance instance)
 	std::vector<VkQueueFamilyProperties> queue_family_properties(queue_family_count);
 	vkGetPhysicalDeviceQueueFamilyProperties(phys_device, &queue_family_count, queue_family_properties.data());
 
+	*family_index = 0;
+
 	float queue_priorities[] = { 1.0f };
 	VkDeviceQueueCreateInfo qci = { VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO };
 	qci.queueFamilyIndex = 0;
 	qci.queueCount = 1;
 	qci.pQueuePriorities = queue_priorities;
 
+	const char *extensions[] = {
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME
+	};
+
 	VkDeviceCreateInfo dci = { VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO };
 	dci.pQueueCreateInfos = &qci;
 	dci.queueCreateInfoCount = 1;
+	dci.enabledExtensionCount = sizeof(extensions) / sizeof(extensions[0]);
+	dci.ppEnabledExtensionNames = extensions; 
 	
 	VkDevice device = VK_NULL_HANDLE;
 	VK_CHECK(vkCreateDevice(phys_device, &dci, 0, &device));
@@ -99,6 +107,44 @@ VkSurfaceKHR create_surface(VkInstance instance, GLFWwindow *window)
 	return surface;
 }
 
+VkSwapchainKHR create_swapchain(VkDevice device, VkSurfaceKHR surface, uint32_t family_idx, uint32_t width, uint32_t height)
+{
+	VkSwapchainKHR swapchain = 0;
+	VkSwapchainCreateInfoKHR scci = { VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR };
+	scci.surface = surface;
+	scci.minImageCount = 2;
+	scci.imageFormat = VK_FORMAT_R8G8B8A8_UNORM;
+	scci.imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
+	scci.imageExtent = VkExtent2D{ width, height };
+	scci.imageArrayLayers = 1;
+	scci.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+	scci.queueFamilyIndexCount = 1;
+	scci.pQueueFamilyIndices = &family_idx;
+	scci.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+	scci.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	scci.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+	VK_CHECK(vkCreateSwapchainKHR(device, &scci, 0, &swapchain));
+	return swapchain;
+}
+
+VkSemaphore create_semaphore(VkDevice device)
+{
+	VkSemaphore semaphore = 0;
+	VkSemaphoreCreateInfo sci = { VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO };
+	VK_CHECK(vkCreateSemaphore(device, &sci, nullptr, &semaphore));
+	return semaphore;
+}
+
+VkCommandPool create_cmd_pool(VkDevice device, uint32_t family_idx)
+{
+	VkCommandPool cmd_pool = 0;
+	VkCommandPoolCreateInfo cpci = { VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO };
+	cpci.queueFamilyIndex = family_idx;
+	cpci.flags = VK_COMMAND_POOL_CREATE_TRANSIENT_BIT;
+	VK_CHECK(vkCreateCommandPool(device, &cpci, nullptr, &cmd_pool));
+	return cmd_pool;
+}
+
 int main()
 {
 	int rc = glfwInit();
@@ -107,7 +153,8 @@ int main()
 	VkInstance instance = create_instance();
 	assert(instance);
 
-	VkDevice device = create_device(instance);
+	uint32_t family_idx;
+	VkDevice device = create_device(instance, &family_idx);
 	assert(device);
 
 	GLFWwindow *window = glfwCreateWindow(1024, 720, "vk_exp", nullptr, nullptr);
@@ -115,9 +162,79 @@ int main()
 
 	VkSurfaceKHR surface = create_surface(instance, window);
 	assert(surface);
+	
+	int w, h;
+	glfwGetWindowSize(window, &w, &h);
+	VkSwapchainKHR swapchain = create_swapchain(device, surface, family_idx, w, h);
+	assert(swapchain);
+
+	VkSemaphore aquire_semaphore = create_semaphore(device);
+	assert(aquire_semaphore);
+	VkSemaphore submit_semaphore = create_semaphore(device);
+	assert(submit_semaphore);
+
+	VkQueue queue;
+	vkGetDeviceQueue(device, family_idx, 0, &queue);
+
+	uint32_t swapchain_image_count;
+	vkGetSwapchainImagesKHR(device, swapchain, &swapchain_image_count, nullptr);
+	std::vector<VkImage> swapchain_images(swapchain_image_count);
+	vkGetSwapchainImagesKHR(device, swapchain, &swapchain_image_count, swapchain_images.data());
+
+	VkCommandPool cmd_pool = create_cmd_pool(device, family_idx);
+	assert(cmd_pool);
+
+	VkCommandBufferAllocateInfo cbi = { VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO };
+	cbi.commandPool = cmd_pool;
+	cbi.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+	cbi.commandBufferCount = 1;
+
+	VkCommandBuffer cmd_buf;
+	VK_CHECK(vkAllocateCommandBuffers(device, &cbi, &cmd_buf));
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+
+		uint32_t image_idx = 0;
+		VK_CHECK(vkAcquireNextImageKHR(device, swapchain, ~0, aquire_semaphore, VK_NULL_HANDLE, &image_idx));
+		VK_CHECK(vkResetCommandPool(device, cmd_pool, 0));
+
+		VkCommandBufferBeginInfo cbbi = { VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO };
+		cbbi.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+		VK_CHECK(vkBeginCommandBuffer(cmd_buf, &cbbi));
+		
+		VkClearColorValue color = { 0.3f, 0.6f, 0.9f, 1.0f };
+		VkImageSubresourceRange range = {};
+		range.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		range.layerCount = 1;
+		range.levelCount = 1;
+		vkCmdClearColorImage(cmd_buf, swapchain_images[image_idx], VK_IMAGE_LAYOUT_GENERAL, &color, 1, &range);
+
+		VK_CHECK(vkEndCommandBuffer(cmd_buf));
+		
+		VkPipelineStageFlags submit_stage_mask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+
+		VkSubmitInfo si = { VK_STRUCTURE_TYPE_SUBMIT_INFO };
+		si.commandBufferCount = 1;
+		si.pCommandBuffers = &cmd_buf;
+		si.waitSemaphoreCount = 1;
+		si.pWaitSemaphores = &aquire_semaphore;
+		si.pWaitDstStageMask = &submit_stage_mask;
+		si.signalSemaphoreCount = 1;
+		si.pSignalSemaphores = &submit_semaphore;
+
+		vkQueueSubmit(queue, 1, &si, VK_NULL_HANDLE);
+	
+		VkPresentInfoKHR pi = { VK_STRUCTURE_TYPE_PRESENT_INFO_KHR };
+		pi.swapchainCount = 1;
+		pi.pSwapchains = &swapchain;
+		pi.pImageIndices = &image_idx;
+		pi.waitSemaphoreCount = 1;
+		pi.pWaitSemaphores = &submit_semaphore;
+
+		VK_CHECK(vkQueuePresentKHR(queue, &pi));
+
+		VK_CHECK(vkDeviceWaitIdle(device));
 	}
 
 	vkDestroyDevice(device, nullptr);
